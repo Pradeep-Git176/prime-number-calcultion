75ab9ace098a6801aa9d82096c314823babea551
Pradeep Kumar <prdp1151@gmail.com>
2022-06-01 08:15:14 +0530

    modified java file

--- src/com/repo/test/PrimeNumber.java
+++ src/com/repo/test/PrimeNumber.java
@@ -1,9 +1,15 @@
 package com.repo.test;
 
+import java.util.Scanner;
+
 public class PrimeNumber {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter a number to be cheked if primer");
+		int num = sc.nextInt();
+		
+		//logic 
 
 	}
 
